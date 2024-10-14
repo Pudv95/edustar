eb4e5e66686858887cbf5ff81c6ec08cbe3b1bf0
pudv95 <vivekupadhayay18@gmail.com>
2024-10-14 09:30:04 +0530

    added meta tags in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
       <body className={`${inter.className}`}>
         <SpeedInsights />
         <Analytics />
